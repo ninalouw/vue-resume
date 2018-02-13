24ab4278616624ff60f020406076cdec609efbb2
Nina Louw <ninalouw@mac.com>
2018-02-12 18:30:33 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Anonymous Pro', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  margin-top: 30px;
+}
+.logo {
+    margin-left:60px;
+    margin-bottom:60px;
+    width: 100px;
+    height: 100px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 hero-text">
+        <h1>{{ title }}</h1>
+        <p> {{ intro }}</p>
+        <button class="btn btn-outline-primary">Email</button>
+        <button class="btn btn-outline-primary">Github</button>
+        <button class="btn btn-outline-primary">LinkedIn</button>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 hero-video">
+        <canvas></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      title: 'Nina Mae Louw',
+      intro: 'I am a full stack developer and designer. I am passionate about creating intuitive user experiences and have the skills to build them: from user research, prototyping,and usability testing to building and deploying full stack applications.',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+font-weight: bold;
+font-family: 'Playfair Display', serif;
+font-size: 5rem;
+}
+p {
+  max-width: 450px;
+  font-size: 2rem;
+  font-family: 'Anonymous Pro', monospace;
+}
+canvas {
+  background-color: #FFC8D9;
+  width: 480px;
+  height: 430px;
+}
+.btn-outline-primary {
+  border: solid 1px #FFC8D9;
+  padding: 8px 16px 8px 16px;
+  margin-right:5px;
+  margin-top:10px;
+  border-radius: 0%;
+}
+button .btn-outline-primary :hover{
+  cursor: pointer;
+  background-color: #FFC8D9;
+  color: #fff;
+}
+</style>
